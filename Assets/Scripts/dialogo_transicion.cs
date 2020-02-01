71bf76c3027b3876d145db0d9150a3b95e245377
JohanEstebanHernandez <47666116+JohanEstebanHernandez@users.noreply.github.com>
2020-01-31 23:12:26 -0500

    pruebas dialogos
    ensayos para script de generacion de dialogos

---
+++ Assets/Scripts/dialogo_transicion.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dialogo_transicion : MonoBehaviour
+{
+
+    public string[] dialogo;
+    public Text texto;
+    public bool activar;
+    Coroutine retraso;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Conversacion()
+    {
+        if (activar)
+        {
+            TerminarDialogo();
+            retraso = StartCoroutine(contar());
+        }
+    }
+
+     IEnumerator contar()
+    {
+        string[] conversar;
+        conversar = dialogo;
+        int oracion = conversar.Length; 
+        string reinicio = "";
+        activar = true;
+        yield return null;
+
+        for (int i = 0; i < oracion; i++)
+        {
+            if (activar)
+            {
+                for (int s = 0; s < conversar[i].Length; s++)
+                {
+                    if (activar)
+                    {
+                        reinicio = string.Concat(reinicio, conversar[i][s]);
+                        new WaitForSeconds(4);
+                    }
+                }
+                TerminarDialogo();
+            }
+        }
+     }
+    public void TerminarDialogo()
+    {
+        activar = false;
+        if(retraso != null)
+        {
+            StopCoroutine(retraso);
+            retraso = null;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other)
+        {
+            Conversacion();
+            TerminarDialogo();
+        }
+    }
+}
---
+++ Assets/Scripts/instanciar_texto.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instanciar_texto : MonoBehaviour
+{
+    public GameObject texto;
+
+    void Start()
+    {
+        GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(texto,new Vector2(50,100),Quaternion.identity);
+    }
+}
