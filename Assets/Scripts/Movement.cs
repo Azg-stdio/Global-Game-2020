74e1ffa3ade5652b609b07cbbac68d7f55952008
miguel_arbelaez <miguel@epica.ai>
2020-01-31 23:09:54 -0500

    Movement Script + Parallax Effect + Simple physics

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
+    bool isgrounded = true;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    bool direction;
+    public float speed = 1;
+    
+    void FixedUpdate()
+    {        
+        if (isgrounded)
+        {
+            int multiplier = 1;
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+            rb.freezeRotation = false;
+            if (moveHorizontal <= 0.0f)
+            {
+                if (!direction)
+                {
+                    rb.velocity = new Vector2(1.0f, 0.0f);
+                    rb.freezeRotation = true;
+                }
+                direction = true;
+            }
+            else
+            {
+                if (direction)
+                {
+                    rb.velocity = new Vector2(1.0f, 0.0f);
+                    rb.freezeRotation = true;
+                }
+                direction = false;
+            }
+
+            this.GetComponent<Rigidbody2D>().AddForce(movement * multiplier * speed * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
     {
-        
+        if (col.gameObject.tag=="Floor")
+        {
+            isgrounded = true;
+        }
+        Debug.Log(isgrounded);
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Floor")
+        {
+            isgrounded = false;
+        }
+        Debug.Log(isgrounded);
     }
 }
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Transform[] backgrounds;
+    private float[] parallaxScales;
+
+    public float smoothing;
+
+    private Transform cam;
+    private Vector3 previousCamPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = Camera.main.transform;
+
+        previousCamPos = cam.position;
+
+        parallaxScales = new float[backgrounds.Length];
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * -1;
+        }
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+
+            float parallax = cam.position.x - (cam.position.x / ((i + 1) * 8));
+            Vector3 pos = new Vector3(parallax, backgrounds[i].position.y, backgrounds[i].position.z);
+            backgrounds[i].position = pos;
+        }
+        previousCamPos = cam.position;
+    }
+}
