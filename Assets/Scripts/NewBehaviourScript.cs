2b85ca8264ae3c149018abff0991507de945bec6
JohanEstebanHernandez <47666116+JohanEstebanHernandez@users.noreply.github.com>
2020-02-02 14:17:36 -0500

    dialogos
    dialogos transferidos

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+1)  > Hola asset inacabado bienvenido a mi paraiso en construcción  \(^ᴗ ^) /
+     > Soy tu programador, algo así como Dios, pero mejor( ͡° ͜ʖ ͡°)
+2)  > Creo que olvidé ponerle collider a algunas cosas ¯\_(e_e)_ /¯
+     > Apreciaría que te quedaras quieto mientras arreglo todo.^ ᴗ ^.
+3)  > Con calma, paso a paso, sin afanes(; º`дº´)
+     > No me obligues a programarte obediencia ⌐▄_▄
+4)  > BUEN INTENTO!  ⌐▄_▄
+     > Tardé programando este puzzle 2 días, apuesto que no podrás resolverlo(> ᴗ <)Ψ
+5)  > Ya sabía yo que te ibas a quedar atorado(─_─;)
+     > No es facil ubicarte con CTRL + Z para arreglar tus desastres(¯―¯;) 
+6)  > Mira! Es una de tus partes, con razón no la encontraba en mis assets... (; º`дº´)
+     > Puedes ayudarme a encontrar el resto ? ('· ω ·')
+    }
+}
