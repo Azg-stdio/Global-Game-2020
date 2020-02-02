3cee5a0584aff709ec510b548f93c2849f22718b
JohanEstebanHernandez <47666116+JohanEstebanHernandez@users.noreply.github.com>
2020-02-02 14:28:25 -0500

    agregados dialogos nuevos

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -13,17 +13,17 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-1)  > Hola asset inacabado bienvenido a mi paraiso en construcción  \(^ᴗ ^) /
-     > Soy tu programador, algo así como Dios, pero mejor( ͡° ͜ʖ ͡°)
-2)  > Creo que olvidé ponerle collider a algunas cosas ¯\_(e_e)_ /¯
-     > Apreciaría que te quedaras quieto mientras arreglo todo.^ ᴗ ^.
-3)  > Con calma, paso a paso, sin afanes(; º`дº´)
-     > No me obligues a programarte obediencia ⌐▄_▄
-4)  > BUEN INTENTO!  ⌐▄_▄
-     > Tardé programando este puzzle 2 días, apuesto que no podrás resolverlo(> ᴗ <)Ψ
-5)  > Ya sabía yo que te ibas a quedar atorado(─_─;)
-     > No es facil ubicarte con CTRL + Z para arreglar tus desastres(¯―¯;) 
-6)  > Mira! Es una de tus partes, con razón no la encontraba en mis assets... (; º`дº´)
-     > Puedes ayudarme a encontrar el resto ? ('· ω ·')
+//1)  > Hola asset inacabado bienvenido a mi paraiso en construcción  \(^ᴗ ^) /
+//     > Soy tu programador, algo así como Dios, pero mejor( ͡° ͜ʖ ͡°)
+//2)  > Creo que olvidé ponerle collider a algunas cosas ¯\_(e_e)_ /¯
+//     > Apreciaría que te quedaras quieto mientras arreglo todo.^ ᴗ ^.
+//3)  > Con calma, paso a paso, sin afanes(; º`дº´)
+//     > No me obligues a programarte obediencia ⌐▄_▄
+//4)  > BUEN INTENTO!  ⌐▄_▄
+//     > Tardé programando este puzzle 2 días, apuesto que no podrás resolverlo(> ᴗ <)Ψ
+//5)  > Ya sabía yo que te ibas a quedar atorado(─_─;)
+//     > No es facil ubicarte con CTRL + Z para arreglar tus desastres(¯―¯;) 
+//6)  > Mira! Es una de tus partes, con razón no la encontraba en mis assets... (; º`дº´)
+//     > Puedes ayudarme a encontrar el resto ? ('· ω ·')
     }
 }
